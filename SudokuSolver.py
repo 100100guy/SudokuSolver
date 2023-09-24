248dd53b83b263c6b6123a1442ff1ddf80a11f82
Md. Nafiu Rahman <nafiu.rahman@gmail.com>
2023-09-24 18:32:26 +0600

    indentation fix

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -2,28 +2,29 @@ import tkinter as tk
 from tkinter import ttk  # Import the ttk module
 
 init_board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7],
 ]
 
 board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7],
 ]
+
 
 def solve(bo):
     find = empty_field(bo)
@@ -31,11 +32,11 @@ def solve(bo):
         return True
     else:
         r, c = find
-    for num in range(1,10):
-        if is_valid_move(bo,r,c,num):
+    for num in range(1, 10):
+        if is_valid_move(bo, r, c, num):
             bo[r][c] = num
-            #print_board(bo)
-            #print()
+            # print_board(bo)
+            # print()
             update_board()
 
             if solve(bo):
@@ -45,13 +46,14 @@ def solve(bo):
 
     return False
 
-def is_valid_move(bo,row,col,num):
-    #check row
+
+def is_valid_move(bo, row, col, num):
+    # check row
     for i in range(len(bo[0])):
         if bo[row][i] == num and i != col:
             return False
 
-    #check column
+    # check column
     for i in range(len(bo)):
         if bo[i][col] == num and i != row:
             return False
@@ -60,28 +62,29 @@ def is_valid_move(bo,row,col,num):
     box_x = col // 3
     box_y = row // 3
 
-    for i in range(3*box_y,3*box_y+3):
-        for j in range(3*box_x,3*box_x+3):
-            if bo[i][j] == num and i != row and j!=col:
+    for i in range(3 * box_y, 3 * box_y + 3):
+        for j in range(3 * box_x, 3 * box_x + 3):
+            if bo[i][j] == num and i != row and j != col:
                 return False
 
     return True
+
 
 def empty_field(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
-                return i,j
+                return i, j
     return None
+
+
 def print_board(bo):
     for row in range(len(bo)):
-
-        if row % 3 == 0 :
+        if row % 3 == 0:
             print("- - - - - - - - - - - - - ")
 
         for col in range(len(bo[0])):
-
-            if col % 3 == 0 :
+            if col % 3 == 0:
                 print("| ", end="")
 
             print(bo[row][col], end=" ")
@@ -90,19 +93,21 @@ def print_board(bo):
 
     print("- - - - - - - - - - - - - ")
 
+
 print_board(board)
 
 # Create a tkinter window
 root = tk.Tk()
 root.title("Sudoku Solver")
 
+
 def update_board():
     for i in range(9):
         for j in range(9):
             value = board[i][j]
             if value != 0:
-                if board[i][j]==init_board[i][j]:
-                    labels[i][j].config(text=str(value),fg="red")
+                if board[i][j] == init_board[i][j]:
+                    labels[i][j].config(text=str(value), fg="red")
                 else:
                     labels[i][j].config(text=str(value))
             else:
@@ -112,12 +117,16 @@ def update_board():
 labels = [[None for _ in range(9)] for _ in range(9)]
 for i in range(9):
     for j in range(9):
-        labels[i][j] = tk.Button(root, text="", width=6, height=3,font=("Helvetica", 12), bg="white")
+        labels[i][j] = tk.Button(
+            root, text="", width=6, height=3, font=("Helvetica", 12), bg="white"
+        )
         labels[i][j].grid(row=i, column=j)
-        
+
+
 def solve_sudoku():
     solve(board)
     update_board()
+
 
 solve_button = tk.Button(root, text="Solve", command=solve_sudoku)
 solve_button.grid(row=10, columnspan=9)
